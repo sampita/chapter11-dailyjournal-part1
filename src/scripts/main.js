e062847256a77b6c02b564f3d1c9dc2ffb8e8fa5
Sam Pita <samanthakpita@gmail.com>
2019-10-30 13:54:05 -0500

    got POST request and subsequent GET request to work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,4 +10,34 @@ import domEntries from "./entriesDOM.js"
 data.getJournalData()
 .then(entries => domEntries.renderJournalEntriesToDom(entries))
 
+//submit button
+const submitButton = document.getElementById("submitButton")
+
+//input field values
+
+//factory function for new journal entry
+const createNewJournalEntry = (dateInput, conceptInput, entryInput, moodInput) => {
+    return {
+        date: dateInput,
+        concept:conceptInput,
+        entryText: entryInput,
+        mood: moodInput
+    }
+}
+
+// createNewJournalEntry()
+//when submit button is clicked, create new journal entry
+submitButton.addEventListener("click", ()=>{
+    const dateInput = document.getElementById("dateInput").value
+    const conceptInput = document.getElementById("conceptInput").value
+    const entryInput = document.getElementById("entryInput").value
+    const moodInput = document.getElementById("moodInput").value
+
+    const newJournalEntry = createNewJournalEntry(dateInput, conceptInput, entryInput, moodInput)
+
+    data.postToApi(newJournalEntry).then(data.getJournalData).then(domEntries.renderJournalEntriesToDom)
+})
+
+
+
 
