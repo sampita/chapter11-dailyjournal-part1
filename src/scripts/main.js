f86c7889ffcf9fc7beeedeecb78002886689c27e
Sam Pita <samanthakpita@gmail.com>
2019-10-30 14:44:35 -0500

    added basic form validation to not allow empty fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,7 +25,7 @@ const createNewJournalEntry = (dateInput, conceptInput, entryInput, moodInput) =
     }
 }
 
-// createNewJournalEntry()
+
 //when submit button is clicked, create new journal entry
 submitButton.addEventListener("click", ()=>{
     const dateInput = document.getElementById("dateInput").value
@@ -33,11 +33,29 @@ submitButton.addEventListener("click", ()=>{
     const entryInput = document.getElementById("entryInput").value
     const moodInput = document.getElementById("moodInput").value
 
+    if(dateInput !== "" && conceptInput !== "" && entryInput !== "" && moodInput !== "") {
+
     const newJournalEntry = createNewJournalEntry(dateInput, conceptInput, entryInput, moodInput)
 
     data.postToApi(newJournalEntry).then(data.getJournalData).then(domEntries.renderJournalEntriesToDom)
-})
+    }
 
-
+    else {window.alert("Please fill out the form completely")}
+  }
+)
+//form validation
+/* function validateForm(dateInput, conceptInput, entryInput, moodInput) {
+    if (dateInput == "") {
+        alert("Please enter the date");
+        return false;
+    } else if (conceptInput == "") {
+        alert("Please enter the concept you covered"); return false;}
+    else if (entryInput == "") {
+        alert("Please input your journal entry"); return false;}
+    else if (moodInput == "") {
+        alert("Please enter your mood");
+        return false;
+    }
+} */
 
 
